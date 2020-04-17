86fd8b7ae5e3a8666de33214034f89d82a0dd7d5
divyasasidharan <49484828+divyasasidharan@users.noreply.github.com>
2020-04-18 01:21:40 +0200

    updated probability calculation

--- HMM_Training.py
+++ HMM_Training.py
@@ -14,6 +14,7 @@ transitionProbabilities_firstOrder={}
 transitionProbabilities_secondOrder={}
 featureCount={}
 emissionProbabilities={}
+Countword={}
 
 def tagCount(corpus):
     """
@@ -23,8 +24,9 @@ def tagCount(corpus):
     """
     tags["START"]=0 
     for i in corpus:
-        tag=i[0] 
-        if "SOS" in i[1]:            
+        #print(i)
+        tag=i[1] 
+        if "START" in i[0]:            
             tags["START"]=tags["START"]+1        
         if tag in tags:
            tags[tag]=tags[tag]+1
@@ -32,35 +34,6 @@ def tagCount(corpus):
            tags[tag]=1    
     return tags
 
-def tagCount_secondOrder(corpus):    
-    """
-    Description:- Build the dictionary with key as 'previous_previous_tag->previous_tag' and value= count of key in corpus.
-    This is for transitionProbability_secondOrder calculation
-    Input :- corpus data read from corpus files 
-    Output :- Dictionary with tag count for second order
-    """
-    print("tagCount_secondOrder")
-    for i in corpus:
-        tag=i[0] 
-        if "SOS" in i[1]:
-            transtioninfo="START"+"->"+"START"
-            if transtioninfo in transition_secondOrder_p:
-                transition_secondOrder_p[transtioninfo]=transition_secondOrder_p[transtioninfo]+1 
-            else:
-                transition_secondOrder_p[transtioninfo]=1 
-            transtioninfo="START"+"->"+tag
-            if transtioninfo in transition_secondOrder_p:
-                transition_secondOrder_p[transtioninfo]=transition_secondOrder_p[transtioninfo]+1 
-            else:
-                transition_secondOrder_p[transtioninfo]=1 
-    for previous_previous, previous in zip(corpus, corpus[1:]):        
-        transtioninfo=previous_previous[0]+"->"+previous[0] # key for transition dictionary
-        
-        if transtioninfo in transition_secondOrder_p:
-            transition_secondOrder_p[transtioninfo]=transition_secondOrder_p[transtioninfo]+1 
-        else:
-            transition_secondOrder_p[transtioninfo]=1 
-    return transition_secondOrder_p
 
 
 def transitionProbability_secondOrder(corpus): 
@@ -70,19 +43,64 @@ def transitionProbability_secondOrder(corpus):
     Input:-corpus data read from corpus files
     Output:- transitionProbabilities for secondOrder
     """
-    print("transitionProbability_secondOrder")
-    for previous_previous,previous, current in zip(corpus, corpus[1:],corpus[2:]): # calculating tag count
-        transtioninfo=previous_previous[0]+"->"+previous[0]+"->"+current[0] # key for transition dictionary        
+    for previous, current in zip(corpus, corpus[1:]):  
+        #print(previous)
+        #print(current)
+        if ("START" in previous[0]):           
+            transtioninfo="START"+"->"+previous[0]+"->"+previous[1]
+            transtioninfo_s=previous[0]+"->"+previous[1]+"->"+current[1]
+        else:
+            if ("START" not in current[0]):
+                transtioninfo=previous[0]+"->"+previous[1]+"->"+current[1]
+            else:
+                transtioninfo=previous[0]+"->"+previous[1]+"->"+"STOP"
+        #print(transtioninfo)
+
         if transtioninfo in transition_secondOrder:
             transition_secondOrder[transtioninfo]=transition_secondOrder[transtioninfo]+1 
         else:
-            transition_secondOrder[transtioninfo]=1      
-    for transitionInfo in transition_secondOrder: 
+            transition_secondOrder[transtioninfo]=1
+
+        if transtioninfo_s in transition_secondOrder:
+            transition_secondOrder[transtioninfo_s]=transition_secondOrder[transtioninfo_s]+1 
+        else:
+            transition_secondOrder[transtioninfo_s]=1
+    #print(transition_secondOrder)
+
+
+    for previous, current in zip(corpus, corpus[1:]):  
+        #print(previous)
+        #print(current)
+        if ("START" in previous[0]):           
+            transtioninfo="START"+"->"+previous[0] 
+            transtioninfo_s=previous[0]+"->"+previous[1]
+        else:
+            if ("START" not in current[0]):
+                transtioninfo=previous[0]+"->"+previous[1]
+            else:
+                transtioninfo=previous[0]+"->"+previous[1]            
+        #print(transtioninfo)
+
+        if transtioninfo in transition_secondOrder_p:
+            transition_secondOrder_p[transtioninfo]=transition_secondOrder_p[transtioninfo]+1 
+        else:
+            transition_secondOrder_p[transtioninfo]=1
+
+        if transtioninfo_s in transition_secondOrder_p:
+            transition_secondOrder_p[transtioninfo_s]=transition_secondOrder_p[transtioninfo_s]+1 
+        else:
+            transition_secondOrder_p[transtioninfo_s]=1
+    #print(transition_secondOrder_p)
+
+    for transitionInfo in transition_secondOrder:
         previous_previousTag=transitionInfo.split("->")[0].strip()
         previousTag=transitionInfo.split("->")[1].strip()
         Tag=previous_previousTag+"->"+previousTag
-        transitionProbabilities_secondOrder[transitionInfo]=(transition_secondOrder[transitionInfo])/(transition_secondOrder_p[Tag])  
-    return transitionProbabilities_secondOrder
+        transitionProbabilities_secondOrder[transitionInfo]=(transition_secondOrder[transitionInfo])/(transition_secondOrder_p[Tag])
+    #print(transition_secondOrder)
+    return transitionProbabilities_secondOrder,transition_secondOrder
+
+
 
 
 def transitionProbability_firstOrder(corpus):  
@@ -92,35 +110,35 @@ def transitionProbability_firstOrder(corpus):
     Output:- transitionProbabilities for firstOrder     
     """
     for i in corpus:
-        tag=i[0] 
-        if "SOS" in i[1]:
+        tag=i[1] 
+        if "START" in i[0]:
             transtioninfo="START"+"->"+tag
             if transtioninfo in transition_firstOrder:
                 transition_firstOrder[transtioninfo]=transition_firstOrder[transtioninfo]+1 
             else:
                 transition_firstOrder[transtioninfo]=1 
-            previous="START"
+            previous=i[0]
             if previous in forwardtagcount:
                 forwardtagcount[previous]= forwardtagcount[previous]+1            
             else:
                 forwardtagcount[previous]=1
     for previous, current in zip(corpus, corpus[1:]):  # calculating tag count        
-        if "SOS" not in current[1]:
-            transtioninfo=previous[0]+"->"+current[0] # key for transition dictionary
+        if "START" not in current[0]:
+            transtioninfo=previous[1]+"->"+current[1] # key for transition dictionary
             if transtioninfo in transition_firstOrder:
                 transition_firstOrder[transtioninfo]=transition_firstOrder[transtioninfo]+1 
             else:
                 transition_firstOrder[transtioninfo]=1  
-            if previous[0] in forwardtagcount:
-                forwardtagcount[previous[0]]= forwardtagcount[previous[0]]+1            
+            if previous[1] in forwardtagcount:
+                forwardtagcount[previous[1]]= forwardtagcount[previous[1]]+1            
             else:
-                forwardtagcount[previous[0]]=1    
+                forwardtagcount[previous[1]]=1    
     #Transition Probability calculation
     for transitionInfo in transition_firstOrder: 
         previousTag=transitionInfo.split("->")[0].strip()
         if forwardtagcount[previousTag]>0:
             transitionProbabilities_firstOrder[transitionInfo]=(transition_firstOrder[transitionInfo])/(forwardtagcount[previousTag])
-    print(forwardtagcount)
+    #print(forwardtagcount)
     return transitionProbabilities_firstOrder,forwardtagcount
 
 def transitionProbability_zeroOrder(corpus):
@@ -130,11 +148,17 @@ def transitionProbability_zeroOrder(corpus):
     Output:- transitionProbabilities for zeroOrder     
     """
     totalCount_N=sum(tags.values())# calculating total count N
+    print("total count is "+str(totalCount_N))
+    print(type(transitionProbabilities_zeroOrder))
     #Transition Probability calculation  
     for tag in tags:
         count=tags[tag]
-        transitionProbabilities_zeroOrder[tag]=(count)/(totalCount_N)
+        value=count/totalCount_N
+        if tag not in transitionProbabilities_zeroOrder:
+            transitionProbabilities_zeroOrder[tag]=value   
     return transitionProbabilities_zeroOrder
+
+
 
 def emissionProbability(corpus):
     """
@@ -142,16 +166,35 @@ def emissionProbability(corpus):
     Input:- corpus data read from corpus files
     Output:- emissionProbabilities     
     """
-    print("emissionProbability")
-    for i in corpus:  # calculating word->Tag count
-        for j in range(0,len(i[1])):
-            featureTag=i[1][j]+"->"+i[0]
+    wordCount={}
+    for i in corpus:
+        if i[2][0] in wordCount:
+            wordCount[i[2][0]]=wordCount[i[2][0]]+1
+        else:
+            wordCount[i[2][0]]=1
+    
+    print("count for _RARE_")
+    for i in corpus:  # calculating word->Tag count for RARE words
+        if wordCount[i[2][0]]<2:
+            featureTag="_RARE_"+"->"+i[1]
             if featureTag not in featureCount:
                 featureCount[featureTag]=1
             else:
-                featureCount[featureTag]=featureCount[featureTag]+1    
+                featureCount[featureTag]=featureCount[featureTag]+1  
+    
+    print("count for features")
+    for i in corpus:  # calculating word->Tag count
+        for j in range(0,len(i[2])):
+            featureTag=i[2][j]+"->"+i[1]
+            if featureTag not in featureCount:
+                featureCount[featureTag]=1
+            else:
+                featureCount[featureTag]=featureCount[featureTag]+1 
+                
+    print("emissionProbability calculation")
     #Calculating Emission Probability
-    for i in featureCount:        
+    for i in featureCount: 
+        #print(i)
         tag=i.split("->")[1].strip()  
         emissionProbabilities[i]=featureCount[i]/tags[tag]
     return emissionProbabilities
