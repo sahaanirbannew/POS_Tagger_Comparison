bc4d64d84ecd0d2fb7a96bf19ece21450bcf46df
divyasasidharan <49484828+divyasasidharan@users.noreply.github.com>
2020-06-10 08:22:41 +0200

    Update accuracy.py
    1.03                                Updated the code for input configuration files without words
    1.04                                Update for F1Score,Recall,Precision using Sklearn

--- accuracy.py
+++ accuracy.py
@@ -1,20 +1,22 @@
-
 """
 Developer:          Divya sasidharan 
 Version:            v1.0 (released)
-Date:               18.04.2020
+Date:               10.06.2020
              
 Description:        Start of Accuracy Check.   
 Version History:
 Version |   Date    |  Change ID |  Changes 
 1.01                                Initial draft version
 1.02                                updated pickle file reader
+1.03                                Updated the code for input configuration files without words
+1.04                                Update for F1Score,Recall,Precision using Sklearn
 """
 import ast
 import math
+import pickle
 import string
 import pandas as pd
-import pickle
+
 
 class Accuracy:
   """
@@ -41,21 +43,26 @@ class Accuracy:
    """
         f=pickle.load(open(self.actualFilePath,"rb"))
         print("------------Actual line------------------")
-        for line in f: 
-            value=line[2][0]+"->"+line[1]
-            self.actualLines.append(value)
-        print(self.actualLines)
+        print(self.actualFilePath)
+        for line in f:            
+            if line[2]!=[]:
+                value=str(line[2])+"===>"+line[1]
+                self.actualLines.append(value)
+        #print(self.actualLines)
 
       
         with open(self.predictedFilePath, 'r') as f:
             print("------------ predicted line------------------")
+            #print(self.predictedFilePath)
             for line in f:
                 line=ast.literal_eval(line)               
                 for i in line:
                     self.predictedLines.append(i)
-            print(self.predictedLines)      
+            #print(self.predictedLines)      
 
         if len(self.actualLines) != len(self.predictedLines):
+            #print(self.actualLines)
+            #print(self.predictedLines)
             print('ERROR: Expected and actual file lengths dont match')            
             return False
 
@@ -73,10 +80,10 @@ class Accuracy:
     predictedArray = []
     actualArray = []
     for i in range(len(self.actualLines)):
-        #print(self.expectedLines[i])
-        currActual= self.actualLines[i].split("->")[1].strip()  
         #print(self.actualLines[i])
-        currPredicted = self.predictedLines[i].split("->")[1].strip()
+        
+        currActual= self.actualLines[i].split("===>")[1].strip()         
+        currPredicted = self.predictedLines[i].split("===>")[1].strip()
         totalcount += 1
         actualArray.append(currActual)
         predictedArray.append(currPredicted)
@@ -88,6 +95,15 @@ class Accuracy:
     print (df_confusion)
     print("accuracy %d/%d: %.2f%%" % (totalmatch, totalcount, 100*(totalmatch/totalcount)))
 
-rawCorpus_test="drf/conf 1/drf_penn_test.pkl"
-rawCorpus_out="output_test/penn_conf 1_output.txt" 
-accuracy = Accuracy(rawCorpus_test, rawCorpus_out)
+    from sklearn.metrics import classification_report,accuracy_score,precision_score,recall_score,f1_score
+    print('Flat Accuracy  :',accuracy_score(actualArray, predictedArray))
+    print("Avg Precision  : " ,precision_score(actualArray, predictedArray, average='weighted'))    
+    print("Avg Recall :  " ,recall_score(actualArray, predictedArray, average='weighted'))
+    print("Avg F1-score :  " ,f1_score(actualArray, predictedArray, average='weighted'))
+    
+    
+
+    print ('Report : ')
+    print (classification_report(actualArray, predictedArray))
+
+#accuracy = Accuracy(rawCorpus_test, rawCorpus_out)
