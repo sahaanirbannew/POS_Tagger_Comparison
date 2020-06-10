40eec8d737b50da54fd8dbb01477373f4daa3474
divyasasidharan <49484828+divyasasidharan@users.noreply.github.com>
2020-06-10 08:15:45 +0200

    Update for input configuration files without words
    1.04 - Updated the code for input configuration files without words

--- HMM_Viterbi_first.py
+++ HMM_Viterbi_first.py
@@ -32,28 +32,31 @@ class ModelDecode:
         self.emissionProbDict = emissionProbDict        
         self.outgoingTagCountDict = forwardtagcount         
         fname="features/"+self.conf+"/"+"features"+"_"+self.corpus+"_train"+".pkl"
+        print(fname)
         self.AllWordsDict_Inv=pickle.load(open(fname,"rb"))        
         self.AllWordsDict={value: key for key, value in self.AllWordsDict_Inv.items()} 
-        print(self.AllWordsDict)
+        #print(self.AllWordsDict)
         tname="tags/"+self.conf+"/"+"tags"+"_"+self.corpus+"_train"+".pkl"
+        print(tname)
         self.tagStateDict_Inv=pickle.load(open(tname,"rb"))       
         self.tagStateDict={value: key for key, value in self.tagStateDict_Inv.items()}
-        print(self.tagStateDict)               
+        #print(self.tagStateDict)               
         values=list(self.tagStateDict.values())
-        print(values)
+        #print(values)
         values.remove("START")
         self.tagStateDict = { i : values[i] for i in range(0, len(values) ) }
-        print(self.tagStateDict)
+        #print(self.tagStateDict)
         self.noOfTag=len(self.tagStateDict)        
         self.rawInput=pickle.load(open(rawcorpus,"rb"))
-        oname="output_test/"+self.corpus+"_"+self.conf+"_output"+".txt"
+        oname="output_test/"+self.corpus+"_"+self.conf+"_firstOrder_output"+".txt"
+        print(oname)
         self.outFile = open(oname,'+w') 
     
     def emission_probability_calculation(self,featureList,tag):
         list_of_emissionKey = []
         #list_of_emissionKey.append(featureList[0]+"->"+tag) 
         for f in featureList[1:]:
-            list_of_emissionKey.append(f+"->"+tag) 
+            list_of_emissionKey.append(f+"===>"+tag) 
         #print(list_of_emissionKey)
         return list_of_emissionKey
 
@@ -68,7 +71,7 @@ class ModelDecode:
         calTransition=0        
         #for words in rawSentence:
             #sentence.append(words)
-        sentence = rawSentence #list of words
+        sentence = [x for x in rawSentence if x != []] #list of features without empty set
         #print("--------------sentence ---------------")
         #print(sentence)
         #print(len(rawSentence))
@@ -79,117 +82,119 @@ class ModelDecode:
         backtrack = [[0 for x in range(length)] for y in range(tagCount)] 
         # THe below lines of code decodes the tags for each first word of the sentence
         
-        for key in self.tagStateDict.keys():       
-            list_of_emissionKey_o=ModelDecode.emission_probability_calculation(self,sentence[0],self.tagStateDict[key]) 
-            #list_of_emissionKey_o=[]# for testing word only 
-            emissionkey_w=sentence[0][0]+"->"+self.tagStateDict[key]            
-            #print(emissionkey)
-            if sentence[0][0] not in self.AllWordsDict.values():
-                #print("word not found : " +sentence[0][0])
-                calEmission=1
-                if not list_of_emissionKey_o:
-                    if wordCount[sentence[0][0]]<2:
-                        #print("Rare words")
-                        #print(sentence[0][0]+":"+str(wordCount[sentence[0][0]]))
-                        emissionkey_rare = "_RARE_" + "->" + self.tagStateDict[key]
-                        #calEmission=1
-                        if emissionkey_rare in self.emissionProbDict:
-                            #print("found emission probability: " +emissionkey_rare)
-                            calEmission=self.emissionProbDict[emissionkey_rare]
-                            #print(calEmission)
+        if length!=0:
+            for key in self.tagStateDict.keys():       
+                list_of_emissionKey_o=ModelDecode.emission_probability_calculation(self,sentence[0],self.tagStateDict[key]) 
+                #list_of_emissionKey_o=[]# for testing word only 
+                emissionkey_w=sentence[0][0]+"===>"+self.tagStateDict[key]            
+                #print(emissionkey)
+                if sentence[0][0] not in self.AllWordsDict.values():
+                    #print("word not found : " +sentence[0][0])
+                    calEmission=1
+                    if not list_of_emissionKey_o:
+                        if wordCount[str(sentence[0])]<2:
+                            #print("Rare words")
+                            #print(sentence[0][0]+":"+str(wordCount[sentence[0][0]]))
+                            emissionkey_rare = "_RARE_" + "===>" + self.tagStateDict[key]
+                            #calEmission=1
+                            if emissionkey_rare in self.emissionProbDict:
+                                #print("found emission probability: " +emissionkey_rare)
+                                calEmission=self.emissionProbDict[emissionkey_rare]
+                                #print(calEmission)                     
                             
-                        
 
+                    else:
+                        #calEmission=1
+                        for emissionkey in list_of_emissionKey_o:                    
+                            if emissionkey in self.emissionProbDict.keys():
+                                calEmission = (calEmission * self.emissionProbDict[emissionkey])
+                elif emissionkey_w not in self.emissionProbDict.keys():                
+                    #print("emissionkey key NOT found")
+                    calEmission=0        
                 else:
-                    #calEmission=1
+                #print("emissionkey key found")
+                    calEmission=self.emissionProbDict[emissionkey_w]
                     for emissionkey in list_of_emissionKey_o:                    
                         if emissionkey in self.emissionProbDict.keys():
-                            calEmission = (calEmission * self.emissionProbDict[emissionkey])
-            elif emissionkey_w not in self.emissionProbDict.keys():                
-                #print("emissionkey key NOT found")
-                calEmission=0        
-            else:
-               #print("emissionkey key found")
-                calEmission=self.emissionProbDict[emissionkey_w]
-                for emissionkey in list_of_emissionKey_o:                    
-                    if emissionkey in self.emissionProbDict.keys():
-                        calEmission =( calEmission * self.emissionProbDict[emissionkey])
-                    #else:
-                        #calEmission=calEmission
-            transitionkey="START"+"->"+self.tagStateDict[key] 
-            #print(transitionkey)
-            #print("final emission Prob: "+str(calEmission))
-            if transitionkey in self.transitionProbDict.keys(): 
-                #print("transition key found")
-                calTransition=self.transitionProbDict[transitionkey]
-            #else:
-                #print("transition key NOT found")
-                #calTransition = 1 / (self.outgoingTagCountDict['START'] + int(self.noOfTag)) #prior probability calculation       
-            viterbi[key][0] = calEmission * calTransition
-            backtrack[key][0] = 0 
-        # The below lines of code decodes the tags for each word after first word
-        for l in range(1,length):#recursion step
-            for tag_to in self.tagStateDict.keys():
-                for tag_from in self.tagStateDict.keys():                    
-                    list_of_emissionKey_o=ModelDecode.emission_probability_calculation(self,sentence[l],self.tagStateDict[tag_to]) 
-                    #list_of_emissionKey_o=[]# for testing word only 
-                    emissionkey_w = sentence[l][0] + "->" + self.tagStateDict[tag_to]
-                    #print(emissionkey)
-                    if sentence[l][0] not in self.AllWordsDict.values():                        
-                        #print("word not found" +sentence[l][0])
-                        calEmission=1
-                        if not list_of_emissionKey_o:
-                            if wordCount[sentence[l][0]]<2:
-                                emissionkey_rare = "_RARE_" + "->" + self.tagStateDict[tag_to]
-                                #calEmission=1
-                                if emissionkey_rare in self.emissionProbDict:
-                                    calEmission=self.emissionProbDict[emissionkey_rare]
+                            calEmission =( calEmission * self.emissionProbDict[emissionkey])
+                        #else:
+                            #calEmission=calEmission
+                transitionkey="START"+"===>"+self.tagStateDict[key] 
+                #print(transitionkey)
+                #print("final emission Prob: "+str(calEmission))
+                if transitionkey in self.transitionProbDict.keys(): 
+                    #print("transition key found")
+                    calTransition=self.transitionProbDict[transitionkey]
+                #else:
+                    #print("transition key NOT found")
+                    #calTransition = 1 / (self.outgoingTagCountDict['START'] + int(self.noOfTag)) #prior probability calculation       
+                viterbi[key][0] = calEmission * calTransition
+                backtrack[key][0] = 0 
+            # The below lines of code decodes the tags for each word after first word
+            for l in range(1,length):#recursion step
+                for tag_to in self.tagStateDict.keys():
+                    for tag_from in self.tagStateDict.keys():                    
+                        list_of_emissionKey_o=ModelDecode.emission_probability_calculation(self,sentence[l],self.tagStateDict[tag_to]) 
+                        #list_of_emissionKey_o=[]# for testing word only 
+                        emissionkey_w = sentence[l][0] + "===>" + self.tagStateDict[tag_to]
+                        #print(emissionkey)
+                        if sentence[l][0] not in self.AllWordsDict.values():                        
+                            #print("word not found" +sentence[l][0])
+                            calEmission=1
+                            if not list_of_emissionKey_o:
+                                if wordCount[str(sentence[l])]<2:
+                                    emissionkey_rare = "_RARE_" + "===>" + self.tagStateDict[tag_to]
                                     #calEmission=1
-                                
+                                    if emissionkey_rare in self.emissionProbDict:
+                                        calEmission=self.emissionProbDict[emissionkey_rare]
+                                        #calEmission=1
+                                    
 
+                            else:
+                                #calEmission=1
+                                for emissionkey in list_of_emissionKey_o:                    
+                                    if emissionkey in self.emissionProbDict.keys():
+                                        calEmission =( calEmission * self.emissionProbDict[emissionkey] )                 
+                        elif emissionkey_w not in self.emissionProbDict.keys():
+                            #print("emissionkey key NOT found")
+                            calEmission =0 
                         else:
-                            #calEmission=1
+                            #print("emissionkey key found")                         
+                            calEmission=self.emissionProbDict[emissionkey_w]
                             for emissionkey in list_of_emissionKey_o:                    
                                 if emissionkey in self.emissionProbDict.keys():
-                                    calEmission =( calEmission * self.emissionProbDict[emissionkey] )                 
-                    elif emissionkey_w not in self.emissionProbDict.keys():
-                        #print("emissionkey key NOT found")
-                        calEmission =0 
-                    else:
-                         #print("emissionkey key found")                         
-                         calEmission=self.emissionProbDict[emissionkey_w]
-                         for emissionkey in list_of_emissionKey_o:                    
-                            if emissionkey in self.emissionProbDict.keys():
-                                calEmission =( calEmission * self.emissionProbDict[emissionkey])
-                            #else:
-                                #calEmission=calEmission
-                    transitionkey = self.tagStateDict[tag_from] + "->" + self.tagStateDict[tag_to]
-                    #print(transitionkey)
-                    if transitionkey in self.transitionProbDict:
-                        #print("transition key NOT found")
-                        #calTransition = 1 / (self.outgoingTagCountDict[self.tagStateDict[tag_from]] + int(self.noOfTag))  #prior probability calculation 
-                                            
-                    #else:
-                        #print("transition key found")
-                        calTransition = self.transitionProbDict[transitionkey]
-                    score = viterbi[tag_from][l-1] * calTransition * calEmission
-                    if score > viterbi[tag_to][l]:
-                        viterbi[tag_to][l] = score
-                        backtrack[tag_to][l] = tag_from
-                    else:
-                        continue
-        best = 0
-        for i in self.tagStateDict.keys():
-            if viterbi[i][length-1] > viterbi[best][length-1]: # get max viterbi[state,Word]
-                best = i
-                
-        path = [sentence[length-1][0]+"->"+self.tagStateDict[best]]        
-        nice_path = [self.tagStateDict[best]]
-        for t in range(length-1, 0, -1):
-            best = backtrack[best][t]            
-            path[0:0] = [sentence[t-1][0]+"->"+self.tagStateDict[best]]  #   for "push"  to first in list since back tracing         
-        return (path)
-
+                                    calEmission =( calEmission * self.emissionProbDict[emissionkey])
+                                #else:
+                                    #calEmission=calEmission
+                        transitionkey = self.tagStateDict[tag_from] + "===>" + self.tagStateDict[tag_to]
+                        #print(transitionkey)
+                        if transitionkey in self.transitionProbDict:
+                            #print("transition key NOT found")
+                            #calTransition = 1 / (self.outgoingTagCountDict[self.tagStateDict[tag_from]] + int(self.noOfTag))  #prior probability calculation 
+                                                
+                        #else:
+                            #print("transition key found")
+                            calTransition = self.transitionProbDict[transitionkey]
+                        score = viterbi[tag_from][l-1] * calTransition * calEmission
+                        if score > viterbi[tag_to][l]:
+                            viterbi[tag_to][l] = score
+                            backtrack[tag_to][l] = tag_from
+                        else:
+                            continue
+            best = 0
+            for i in self.tagStateDict.keys():
+                if viterbi[i][length-1] > viterbi[best][length-1]: # get max viterbi[state,Word]
+                    best = i
+                    
+            path = [str(sentence[length-1])+"===>"+self.tagStateDict[best]]        
+            nice_path = [self.tagStateDict[best]]
+            for t in range(length-1, 0, -1):
+                best = backtrack[best][t]            
+                path[0:0] = [str(sentence[t-1])+"===>"+self.tagStateDict[best]]  #   for "push"  to first in list since back tracing         
+            return (path)
+        else:
+            path=[]
+            return(path)
     def decode(self):
         """
         Description:- Start of decoding .Calls viterbi() method for each sentence in corpus
@@ -202,10 +207,10 @@ class ModelDecode:
         start=0 
         wordCount={}
         for line in self.rawInput: 
-            if line[2][0] in wordCount:
-                wordCount[line[2][0]]=wordCount[line[2][0]]+1
+            if str(line[2]) in wordCount:
+                wordCount[str(line[2])]=wordCount[str(line[2])]+1
             else:
-                wordCount[line[2][0]]=1
+                wordCount[str(line[2])]=1
         #print(wordCount)     
         for line in  self.rawInput: 
             if "START" in line[0]:                
