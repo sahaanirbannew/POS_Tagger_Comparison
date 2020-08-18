92c4c9bab0b34c5de2905609f2bef2aadc56b6d9
divyasasidharan <49484828+divyasasidharan@users.noreply.github.com>
2020-08-18 14:52:19 +0200

    Update HMM_Main.py
    changes for cross corpus test and train

--- HMM_Main.py
+++ HMM_Main.py
@@ -1,7 +1,7 @@
 """
 Developer:          Divya sasidharan 
 Version:            v1.0 (released)
-Date:               10.06.2020
+Date:               17.08.2020
              
 Description:        Start of HMM model training and decoding.   
 Version History:
@@ -10,14 +10,16 @@ Version |   Date    |  Change ID |  Changes
 1.02                                Updated the calls for zero and second order
 1.03                                Updated the calls for zero and second order
 1.04                                Updated the code for input configuration files without words
+1.05                                Updated the code for cross corpus training and testing
 """
-
 import os
 import time 
 import pickle
-import HMM_Training as train
+import HMM_Training_v1 as train
 import HMM_Viterbi_zero as decode_0
-import HMM_Viterbi_first as decode_1
+import HMM_Viterbi_first as decode_1 
+#import hmm_viterbi_zero_v1 as decode_0
+#import HMM_Viterbi_first_v2 as decode_1 # for last 3 conf
 import HMM_Viterbi_second as decode_2
 
 
@@ -38,9 +40,10 @@ def open_file(configNo,corpus_name,dataset):
          
         
         
-def main(configNo,tagger,corpus_name):
+def main(configNo,tagger,corpus_name,corpus_name_test):
     """The main function for HMM Model."""
-    print("Start of HMM_Main")    
+    print("Start of HMM_Main")  
+    
     tagger=tagger
     corpus_name=corpus_name
     start_time = time.time()
@@ -54,9 +57,9 @@ def main(configNo,tagger,corpus_name):
             emissionProbability_out=train.emissionProbability(corpus,configNo)            
             print("Training completed!!")
             dataset="test"            
-            name="drf/"+configNo+"/"+"drf"+"_"+corpus_name+"_"+dataset+".pkl"
+            name="drf/"+configNo+"/"+"drf"+"_"+corpus_name_test+"_"+dataset+".pkl"
             print(name)
-            test=decode_0.ModelDecode(corpus_name,configNo,name,transitionProbability_out,emissionProbability_out,tagCount_out) 
+            test=decode_0.ModelDecode(corpus_name,corpus_name_test,configNo,name,transitionProbability_out,emissionProbability_out,tagCount_out) 
             test.decode() 
         elif int(tagger)==1:
             print("Start of training for first order ")
@@ -70,8 +73,8 @@ def main(configNo,tagger,corpus_name):
             #print(emissionProbability_out)
             print("Training completed!!") 
             dataset="test"
-            name="drf/"+configNo+"/"+"drf"+"_"+corpus_name+"_"+dataset+".pkl"
-            test=decode_1.ModelDecode(corpus_name,configNo,name,transitionProbability_out,emissionProbability_out,forwardtagcount_out) 
+            name="drf/"+configNo+"/"+"drf"+"_"+corpus_name_test+"_"+dataset+".pkl"
+            test=decode_1.ModelDecode(corpus_name,corpus_name_test,configNo,name,transitionProbability_out,emissionProbability_out,forwardtagcount_out) 
             test_output=test.decode()
         elif int(tagger)==2:
             print("Start of training for second order ")
@@ -82,8 +85,8 @@ def main(configNo,tagger,corpus_name):
             emissionProbability_out=train.emissionProbability(corpus,configNo)            
             print("Training completed!!")
             dataset="test"
-            name="drf/"+configNo+"/"+"drf"+"_"+corpus_name+"_"+dataset+".pkl"
-            test=decode_2.ModelDecode(corpus_name,configNo,name,transitionProbability_out,emissionProbability_out,forwardtagcount_out)
+            name="drf/"+configNo+"/"+"drf"+"_"+corpus_name_test+"_"+dataset+".pkl"
+            test=decode_2.ModelDecode(corpus_name,corpus_name_test,configNo,name,transitionProbability_out,emissionProbability_out,forwardtagcount_out)
             test_output=test.decode()                   
         else:
             print("Not a valid input")      
@@ -91,4 +94,4 @@ def main(configNo,tagger,corpus_name):
     else:
         print("Invalid corpus name")
     
-#main("conf 1",2,"penn")# Example to run the code
+#main("conf 1",2,"penn","penn")# Example to run the code
